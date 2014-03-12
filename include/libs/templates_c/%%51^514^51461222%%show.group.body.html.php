00ec7f278734672da7efd45fe92ced065ffb50ce
silago <silago.nevermind@gmail.com>
2014-03-12 23:17:56 +0900

    //

--- include/libs/templates_c/%%51^514^51461222%%show.group.body.html.php
+++ include/libs/templates_c/%%51^514^51461222%%show.group.body.html.php
@@ -1,10 +1,13 @@
-<?php /* Smarty version 2.6.16, created on 2014-03-11 14:50:56
+<?php /* Smarty version 2.6.16, created on 2014-03-12 23:13:44
          compiled from ru/modules/catalog/index/show.group.body.html */ ?>
+<?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
+smarty_core_load_plugins(array('plugins' => array(array('function', 'show_menu', 'ru/modules/catalog/index/show.group.body.html', 1, false),)), $this); ?>
 <?php if ($this->_tpl_vars['items']): ?> <?php $_from = $this->_tpl_vars['items']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }if (count($_from)):
     foreach ($_from as $this->_tpl_vars['key'] => $this->_tpl_vars['item']):
 ?>                     	<div class="product-block">                            <a href="<?php echo $this->_tpl_vars['item']['uri']; ?>
-"><img src="/userfiles/catalog/1cbitrix/<?php echo $this->_tpl_vars['item']['filename']; ?>
+"><img src="/userfiles/catalog/1cbitrix/<?php echo $this->_tpl_vars['item']['thumb']; ?>
 " height="142" width="142" alt="" /></a>							<p><a href="<?php echo $this->_tpl_vars['item']['uri']; ?>
 "><?php echo $this->_tpl_vars['item']['name']; ?>
-</a></p>							</div><?php endforeach; endif; unset($_from); ?><?php endif; ?><?php if ($this->_tpl_vars['group']['description']): ?><div class="about-content">						<div class="about-block">							<p><a class="time" href="#">Режим работы</a></p>								<p><a class="delivery" href="#">Доставка</a></p>							<p><a class="pay" href="#">Оплата</a></p>						</div>						<div class="product-text">						<?php echo $this->_tpl_vars['group']['description']; ?>
+</a></p>							</div><?php endforeach; endif; unset($_from); ?><?php endif; ?><?php if ($this->_tpl_vars['group']['description']): ?><div class="about-content">						<div class="about-block">							  <?php echo smarty_function_show_menu(array('menuid' => 7), $this);?>
+						</div>						<div class="product-text">						<?php echo $this->_tpl_vars['group']['description']; ?>
                         </div>					</div><?php endif; ?>
